aba1006b1643a323560d9d43aa1f63525af813db
kasasSulaiman <s195462@student.dtu.dk>
2020-11-10 23:33:48 +0100

    added if statement

---
+++ MyFirstPlayground.playground/Pages/ifStatement.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+let salary = 2000
+
+if salary >= 2000 {
+    
+    print("grater than 2000")
+}else{
+    
+    print("less than or equal 2000 ")
+    
+}
+
+var myAge = 17
+
+if myAge <= 17 {
+    print("Sorry not allowed. Your age \(myAge)")
+} else {
+    print("it is allowed")
+}
+
+
+let username = "Sulaiman"
+let password = "123456"
+
+if username == "Sulaiman" && password == "123456"  {
+    print("Welcome Mr. \(username)")
+} else {
+    print("Sorry wrong password or username")
+}
+
